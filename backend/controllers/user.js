32f97d03a2b493d6e09c8951eeea8b84d683827c
Ange PA <ange.lef202209@gmail.com>
2023-11-01 18:43:03 +0100

    Setting up -user login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,12 @@
 /************************************************************/
 /************** Controller user *****************************/
 /************************************************************/
-const bcrypt = require('bcrypt')
-const User = require('../models/User')
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+// variable d'environnement
+const keyToken = process.env.KEY_TOKEN
 
 //fonction de création d'un compte
 exports.signup = (req, res) => {
@@ -18,3 +22,31 @@ exports.signup = (req, res) => {
         })
         .catch(error => res.status(500).json({error}))
 }
+
+// Connecter un utilisateur existant //
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token : jwt.sign(
+                          { userId: user._id },
+                          `${keyToken}`,
+                          { expiresIn: '24h' }
+                      )
+                    });
+                })
+                .catch(error => {
+                  res.status(500).json({ error })});
+        })
+        .catch(error =>{
+          res.status(500).json({ error })});
+  };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,13 @@
 /************************************************************/
 /***************** Routes user *****************************/
 /************************************************************/
-const express = require('express')
-const router = express.Router()
-const userCtrl = require('../controllers/user')
+const express = require('express');
+const router = express.Router();
+const userCtrl = require('../controllers/user');
 
 // inscription user routes //
-router.post ('/signup', userCtrl.signup)
+router.post ('/signup', userCtrl.signup);
+// connexion user routes //
+router.post ('/login', userCtrl.login)
 
 module.exports = router
